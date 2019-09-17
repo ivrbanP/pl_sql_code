7273b78c464ea626d0806f4b69d6b95ba9b038cf
Ivo Vrban <ivo.vrban@gmail.com>
2019-09-17 22:52:48 +0200

    extended code

--- divisible_by_3_or_5.sql
+++ divisible_by_3_or_5.sql
@@ -3,23 +3,31 @@ create table run_results (iterator number(20,0), mod_3 char(1), mod_5 char(5),  
 
 declare
     suma number := 0;
-    mod_bool_3 char(1);
-    mod_bool_5 char(1);
+    mod_bool_3 char(1):='F';
+    mod_bool_5 char(1):='F';
 begin
     for i in 1..1000 loop
         if (i mod 3) = 0 or (i mod 5) = 0 
         then 
             suma:=suma+i;
-            case 
-                when (i mod 3)=0 then 
-                    mod_bool_3:='T'; mod_bool_5:='F';
-                when (i mod 5)=0 then 
-                    mod_bool_3:='F'; mod_bool_5:='T';
-            end case;
+            if (i mod 3)=0 then 
+                mod_bool_3:='T'; 
+                if (i mod 5)=0 then 
+                    mod_bool_5:='T';
+                end if;
+            elsif (i mod 5)=0 then
+                mod_bool_5:='T';
+                if (i mod 3)=0 then 
+                    mod_bool_3:='T';
+                end if;
+            else 
+                mod_bool_3:='F'; mod_bool_5:='F';
+            end if;
         else 
             suma:=suma+0; 
         end if;
         insert into run_results values (i, mod_bool_3, mod_bool_5, suma);
+        mod_bool_3:='F'; mod_bool_5:='F';
     end loop;
     
     commit;
@@ -28,9 +36,14 @@ end;
 /
 
 select * from 
-run_results;
+run_results order by agg ;
 
 
-select decode ( (1=1),0,'T','F' )  from dual;
+select * from 
+run_results 
+where mod_3='T' and mod_5='T'
+order by agg desc;
+
+
 
 
